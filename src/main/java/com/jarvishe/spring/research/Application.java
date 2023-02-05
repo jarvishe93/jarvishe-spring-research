f05456c3b05cbd8ee4c68be22babf5aa8e69f323
heguoliang <jarvishe93@163.com>
2023-02-05 19:19:20 +0800

    testAnnotationConfigServletWebServerApplicationContext

--- src/main/java/com/jarvishe/spring/research/Application.java
+++ src/main/java/com/jarvishe/spring/research/Application.java
@@ -1,5 +1,7 @@
 package com.jarvishe.spring.research;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,8 +13,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class Application {
+    static Logger logger = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
+        logger.info("test");
         SpringApplication.run(Application.class, args);
     }
 
--- src/main/java/com/jarvishe/spring/research/p002/ApplicationContextTest.java
+++ src/main/java/com/jarvishe/spring/research/p002/ApplicationContextTest.java
@@ -2,12 +2,18 @@ package com.jarvishe.spring.research.p002;
 
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletRegistrationBean;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.context.AnnotationConfigServletWebServerApplicationContext;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.mvc.Controller;
 
 import java.util.Arrays;
 
@@ -23,9 +29,13 @@ public class ApplicationContextTest {
         // testClassPathXmlApplicationContext();
         // testSelfReadClassPathXmlApplicationContext();
         // testFileSystemXmlApplicationContext();
-        testAnnotationConfigApplicationContext();
+        // testAnnotationConfigApplicationContext();
+        testAnnotationConfigServletWebServerApplicationContext();
     }
 
+    /**
+     * 手动扫描classpath下的xml文件，添加bean定义
+     */
     static void testSelfReadClassPathXmlApplicationContext() {
         DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
         System.out.println("beanFactory 初始 bean names");
@@ -37,23 +47,76 @@ public class ApplicationContextTest {
         Arrays.stream(beanFactory.getBeanDefinitionNames()).forEach(System.out::println);
     }
 
+    /**
+     * 经典容器，基于classpath下的xml文件创建
+     */
     static void testClassPathXmlApplicationContext() {
         ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("b01.xml");
         Arrays.stream(classPathXmlApplicationContext.getBeanDefinitionNames()).forEach(System.out::println);
         System.out.println(classPathXmlApplicationContext.getBean(Bean2.class).getBean1());
     }
 
+    /**
+     * 经典容器，基于filepath下的xml文件创建
+     */
     static void testFileSystemXmlApplicationContext() {
         FileSystemXmlApplicationContext fileSystemXmlApplicationContext = new FileSystemXmlApplicationContext("src/main/resources/b01.xml");
         Arrays.stream(fileSystemXmlApplicationContext.getBeanDefinitionNames()).forEach(System.out::println);
         System.out.println(fileSystemXmlApplicationContext.getBean(Bean2.class).getBean1());
     }
 
+    /**
+     * 经典容器，基于java配置类创建
+     */
     static void testAnnotationConfigApplicationContext() {
         AnnotationConfigApplicationContext annotationConfigApplicationContext
                 = new AnnotationConfigApplicationContext(AnnotationConfig.class);
         Arrays.stream(annotationConfigApplicationContext.getBeanDefinitionNames()).forEach(System.out::println);
         System.out.println(annotationConfigApplicationContext.getBean(Bean2.class).getBean1());
+    }
+
+    /**
+     * 经典容器，基于java配置类创建，用于web环境
+     */
+    static void testAnnotationConfigServletWebServerApplicationContext() {
+        AnnotationConfigServletWebServerApplicationContext servletWebServerApplicationContext = new AnnotationConfigServletWebServerApplicationContext(WebConfig.class);
+
+    }
+
+    @Configuration
+    static class WebConfig {
+        /**
+         * 创建容器
+         */
+        @Bean
+        public ServletWebServerFactory servletWebServerFactory() {
+            return new TomcatServletWebServerFactory(8090);
+        }
+
+        /**
+         * 创建dispatcherServlet
+         */
+        @Bean
+        public DispatcherServlet dispatcherServlet() {
+            return new DispatcherServlet();
+        }
+
+        /**
+         * DispatcherServlet 关联 容器
+         */
+        @Bean
+        public DispatcherServletRegistrationBean dispatcherServletRegistrationBean(DispatcherServlet dispatcherServlet) {
+            return new DispatcherServletRegistrationBean(dispatcherServlet, "/");
+        }
+
+        // 测试web类
+        @Bean("/hello")
+        public Controller controller1() {
+            return (request, response) -> {
+                response.getWriter().println("HelloWorld !");
+                return null;
+            };
+        }
     }
 
     @Configuration
